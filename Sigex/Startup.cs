7f0ee42c1c8b189ac55ecc61eda62078e07ce58a
Abel Gonzalez Rodriguez <abelgr45@gmail.com>
2019-06-22 10:23:49 -0400

    Login

---
+++ Sigex/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sigex.Startup))]
+namespace Sigex
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
